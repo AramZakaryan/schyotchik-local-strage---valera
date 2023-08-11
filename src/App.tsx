a6949912283b538f629e6b91bfde72577951f6e8
AramZakaryan <aram.h.zakaryan@gmail.com>
2023-08-11 11:17:08 +0400

    Result of Lesson 4 (useEffect)

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,35 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import "./App.css"
 
 const App = () => {
     const [value, setValue] = useState<number>(0)
 
+    useEffect(() => {
+            let CurValueString = localStorage.getItem("CurValue")
+            if (CurValueString) {
+                setValue(JSON.parse(CurValueString))
+            }
+        },
+        []
+    )
+
+
     const incHandler = () => {
         setValue(value + 1)
     }
-    const setToSessionStorageHandler = () => {
-        // localStorage.setItem("CurValue", JSON.stringify(value))
-        sessionStorage.setItem("CurValue", JSON.stringify(value))
-    }
 
-    const getSessionStorageHandler = () => {
-        // let CurValueString = localStorage.getItem("CurValue")
-        let CurValueString = sessionStorage.getItem("CurValue")
-        if (CurValueString) {
-            setValue(JSON.parse(CurValueString))
-        }
-    }
+    useEffect(() => {
+            localStorage.setItem("CurValue", JSON.stringify(value))
+        },
+        [value]
+    )
 
-    const clearSessionStorageHandler = () => {
-        // localStorage.clear()
-        sessionStorage.clear()
-        setValue(0)
-    }
 
-    const removeSessionStoragehandler = () => {
-        // localStorage.removeItem("CurValue")
-        sessionStorage.removeItem("CurValue")
-
-    }
 
     return (
         <div>
             <h1>{value}</h1>
             <button onClick={incHandler}>increment</button>
-            <button onClick={setToSessionStorageHandler}>set SessionStorage</button>
-            <button onClick={getSessionStorageHandler}>get SessionStorage</button>
-            <br/>
-            <button onClick={clearSessionStorageHandler}>clear SessionStorage</button>
-            <button onClick={removeSessionStoragehandler}>remove SessionStorage (CurValue)</button>
         </div>
     );
 }
